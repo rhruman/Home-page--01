29b9bd397fdae0c190539320497a6ab10942a74c
Robiul Hasan Ruman <78135575+rhruman@users.noreply.github.com>
2021-10-08 19:27:58 +0600

    Update apps.js

--- apps.js
+++ apps.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
     $(".first-owl").owlCarousel({
         items:1,
         navText: ["<img src='prev-icon.png'>","<img src='next-icon.png'>"],
-        // autoplay:true,
-        // autoplayTimeout:1500,
-        // loop:true,
+         autoplay:true,
+         autoplayTimeout:2000,
+         loop:true,
         responsive:{
             0:{
                 nav:false,
